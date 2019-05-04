f0cfe1957a559100815424c4a32839fcdbb4a646
Ahmet Akif Uğurtan <akif.ugurtan@ug.bilkent.edu.tr>
2019-05-04 17:12:31 +0300

    Old Camera Screen

--- src/screens/CameraScreen.js
+++ src/screens/CameraScreen.js
@@ -1,380 +1,77 @@
-/* eslint-disable no-console */
-import RNFetchBlob from 'rn-fetch-blob';
 import React from 'react';
-import {
-  StyleSheet,
-  Text,
-  View,
-  TouchableOpacity,
-  Slider,
-  TouchableWithoutFeedback,
-  Dimensions,
-} from 'react-native';
-// eslint-disable-next-line import/no-unresolved
-import { RNCamera } from 'react-native-camera';
+import {Dimensions, StyleSheet, Text, View} from 'react-native';
+import Camera from 'react-native-camera';
+import RNFetchBlob from 'react-native-fetch-blob';
 
 const azureFaceAPI = 'https://westeurope.api.cognitive.microsoft.com/face/v1.0/detect?returnFaceId=true&returnFaceLandmarks=false&returnFaceAttributes=age,gender,smile,emotion,hair';
 
-const flashModeOrder = {
-  off: 'on',
-  on: 'auto',
-  auto: 'torch',
-  torch: 'off',
-};
+class CameraScreen extends React.Component {
 
-const wbOrder = {
-  auto: 'sunny',
-  sunny: 'cloudy',
-  cloudy: 'shadow',
-  shadow: 'fluorescent',
-  fluorescent: 'incandescent',
-  incandescent: 'auto',
-};
-
-const landmarkSize = 2;
-
-export default class CameraScreen extends React.Component {
-  state = {
-    flash: 'off',
-    zoom: 0,
-    autoFocus: 'on',
-    autoFocusPoint: {
-      normalized: { x: 0.5, y: 0.5 }, // normalized values required for autoFocusPointOfInterest
-      drawRectPosition: {
-        x: Dimensions.get('window').width * 0.5 - 32,
-        y: Dimensions.get('window').height * 0.5 - 32,
-      },
-    },
-    depth: 0,
-    type: 'front',
-    whiteBalance: 'auto',
-    ratio: '16:9',
-  };
-
-  toggleFacing() {
-    this.setState({
-      type: this.state.type === 'back' ? 'front' : 'back',
-    });
+  constructor(props) {
+    super(props);
   }
 
-  toggleFlash() {
-    this.setState({
-      flash: flashModeOrder[this.state.flash],
-    });
-  }
+  takePicture() {
+    this.camera.capture().then((data) => {
 
-  toggleWB() {
-    this.setState({
-      whiteBalance: wbOrder[this.state.whiteBalance],
-    });
-  }
+      console.log(data);
 
-  toggleFocus() {
-    this.setState({
-      autoFocus: this.state.autoFocus === 'on' ? 'off' : 'on',
-    });
-  }
-
-  touchToFocus(event) {
-    const { pageX, pageY } = event.nativeEvent;
-    const screenWidth = Dimensions.get('window').width;
-    const screenHeight = Dimensions.get('window').height;
-    const isPortrait = screenHeight > screenWidth;
-
-    let x = pageX / screenWidth;
-    let y = pageY / screenHeight;
-    // Coordinate transform for portrait. See autoFocusPointOfInterest in docs for more info
-    if (isPortrait) {
-      x = pageY / screenHeight;
-      y = -(pageX / screenWidth) + 1;
-    }
-
-    this.setState({
-      autoFocusPoint: {
-        normalized: { x, y },
-        drawRectPosition: { x: pageX, y: pageY },
-      },
-    });
-  }
-
-  zoomOut() {
-    this.setState({
-      zoom: this.state.zoom - 0.1 < 0 ? 0 : this.state.zoom - 0.1,
-    });
-  }
-
-  zoomIn() {
-    this.setState({
-      zoom: this.state.zoom + 0.1 > 1 ? 1 : this.state.zoom + 0.1,
-    });
-  }
-
-  setFocusDepth(depth) {
-    this.setState({
-      depth,
-    });
-  }
-
-
- /* onTouchStartRecording() {
-    this.camera.recordAsync().then((data) => {
-      const videoPath = data.uri;
-      const header = {}; // maybe you need authentication here
-      fetchBlob.fetch('PUT', 'your-url', headers, fetchBlob.wrap(stripFilePathPrefix(payload))).then(() => {});
-    });
-  }*/
-
-
-  takePicture = async function(camera) {
-    const options = { quality: 0.5, base64: true };
-    const data = await this.camera.takePictureAsync(options);
-    //  eslint-disable-next-line
-    /*console.warn('1', data.uri);
-    console.warn('2', data.base64);
-    console.warn('3', RNFetchBlob.wrap(stripFilePathPrefix(data.uri)));
-    console.warn('4', RNFetchBlob.wrap(data.uri));
-    const picPath = data.uri;*/
-
-    RNFetchBlob.fetch('POST', azureFaceAPI, {
+      RNFetchBlob.fetch('POST', azureFaceAPI, {
         'Content-Type': 'application/octet-stream',
         'Ocp-Apim-Subscription-Key': '6163804f762148a3b9f67b09a6b95e8e',
         // Change BASE64 encoded data to a file path with prefix `RNFetchBlob-file://`.
         // Or simply wrap the file path with RNFetchBlob.wrap().
-        }, RNFetchBlob.wrap(data.uri)).then((res) => {
-            console.log(res.json());
-            this.props.navigation.navigate('MoodDetectScreen',
-            data: {img: data.path, mood: res.json()}});
-        });
+      }, RNFetchBlob.wrap(data.path)).then((res) => {
+        console.log(res.json());
+        this.props.navigation.navigate('MoodDetectScreen',
+            {data: {img: data.path, mood: res.json()}});
+      }).catch((err) => {
+        // error handling ..
+        console.log(err);
+      });
 
-  };
-
-  toggle = value => () => this.setState(prevState => ({ [value]: !prevState[value] }));
-
-  renderTextBlock = ({ bounds, value }) => (
-    <React.Fragment key={value + bounds.origin.x}>
-      <Text style={[styles.textBlock, { left: bounds.origin.x, top: bounds.origin.y }]}>
-        {value}
-      </Text>
-      <View
-        style={[
-          styles.text,
-          {
-            ...bounds.size,
-            left: bounds.origin.x,
-            top: bounds.origin.y,
-          },
-        ]}
-      />
-    </React.Fragment>
-  );
-
-  textRecognized = object => {
-    const { textBlocks } = object;
-    this.setState({ textBlocks });
-  };
-
-  barcodeRecognized = ({ barcodes }) => this.setState({ barcodes });
-
-
-  renderBarcode = ({ bounds, data, type }) => (
-    <React.Fragment key={data + bounds.origin.x}>
-      <View
-        style={[
-          styles.text,
-          {
-            ...bounds.size,
-            left: bounds.origin.x,
-            top: bounds.origin.y,
-          },
-        ]}
-      >
-        <Text style={[styles.textBlock]}>{`${data} ${type}`}</Text>
-      </View>
-    </React.Fragment>
-  );
-
-  renderCamera() {
-    const { canDetectFaces, canDetectText, canDetectBarcode } = this.state;
-
-    const drawFocusRingPosition = {
-      top: this.state.autoFocusPoint.drawRectPosition.y - 32,
-      left: this.state.autoFocusPoint.drawRectPosition.x - 32,
-    };
-    return (
-      <RNCamera
-        ref={ref => {
-          this.camera = ref;
-        }}
-        style={{
-          flex: 1,
-          justifyContent: 'space-between',
-        }}
-        //captureTarget={RNCamera.constants.CaptureTarget.disk}
-        type={this.state.type}
-        flashMode={this.state.flash}
-        autoFocus={this.state.autoFocus}
-        autoFocusPointOfInterest={this.state.autoFocusPoint.normalized}
-        zoom={this.state.zoom}
-        whiteBalance={this.state.whiteBalance}
-        ratio={this.state.ratio}
-        focusDepth={this.state.depth}
-        permissionDialogTitle={'Permission to use camera'}
-        permissionDialogMessage={'We need your permission to use your camera phone'}
-        faceDetectionLandmarks={
-          RNCamera.Constants.FaceDetection.Landmarks
-            ? RNCamera.Constants.FaceDetection.Landmarks.all
-            : undefined
-        }
-        onFacesDetected={canDetectFaces ? this.facesDetected : null}
-        onTextRecognized={canDetectText ? this.textRecognized : null}
-        onGoogleVisionBarcodesDetected={canDetectBarcode ? this.barcodeRecognized : null}
-      >
-        <View style={StyleSheet.absoluteFill}>
-          <View style={[styles.autoFocusBox, drawFocusRingPosition]} />
-          <TouchableWithoutFeedback onPress={this.touchToFocus.bind(this)}>
-            <View style={{ flex: 1 }} />
-          </TouchableWithoutFeedback>
-        </View>
-
-
-        <View
-          style={{
-            flex: 0.5,
-            height: 72,
-            backgroundColor: 'transparent',
-            flexDirection: 'row',
-            justifyContent: 'space-around',
-          }}
-        >
-        </View>
-
-        <View style={{ flex: 1, bottom: 0, justifyContent: 'center' }}>
-          <View
-            style={{
-              height: 56,
-              backgroundColor: 'transparent',
-              flexDirection: 'row',
-              alignSelf: 'flex-end',
-            }}
-          >
-            <TouchableOpacity
-              style={[styles.flipButton, { flex: 0.1, alignSelf: 'flex-end' }]}
-              onPress={this.zoomIn.bind(this)}
-            >
-              <Text style={styles.flipText}> + </Text>
-            </TouchableOpacity>
-            <TouchableOpacity
-              style={[styles.flipButton, { flex: 0.1, alignSelf: 'flex-end' }]}
-              onPress={this.zoomOut.bind(this)}
-            >
-              <Text style={styles.flipText}> - </Text>
-            </TouchableOpacity>
-
-            <TouchableOpacity
-              style={[styles.flipButton, styles.picButton, { flex: 0.3, alignSelf: 'flex-end' }]}
-              onPress={this.takePicture.bind(this)}
-            >
-              <Text style={styles.flipText}> SNAP </Text>
-
-            </TouchableOpacity>
-          </View>
-        </View>
-
-
-      </RNCamera>
-    );
+    }).catch(err => console.error(err));
   }
 
   render() {
-    return <View style={styles.container}>{this.renderCamera()}</View>;
+    return (
+        <Camera ref={cam => { this.camera = cam; }} style={styles.preview}
+                aspect={Camera.constants.Aspect.fill} type='front'
+                captureTarget={Camera.constants.CaptureTarget.temp}
+                fixOrientation={true}>
+          <View style={styles.circle}/>
+          <Text style={styles.capture}
+                onPress={this.takePicture.bind(this)}> [CAPTURE]
+          </Text>
+        </Camera>
+    );
   }
+
 }
 
 const styles = StyleSheet.create({
-  container: {
+  preview: {
     flex: 1,
-    paddingTop: 10,
-    backgroundColor: '#000',
-  },
-  flipButton: {
-    flex: 0.3,
-    height: 40,
-    marginHorizontal: 2,
-    marginBottom: 10,
-    marginTop: 10,
-    borderRadius: 8,
-    borderColor: 'white',
-    borderWidth: 1,
-    padding: 5,
+    justifyContent: 'flex-end',
     alignItems: 'center',
-    justifyContent: 'center',
+    height: Dimensions.get('window').height,
+    width: Dimensions.get('window').width,
   },
-  autoFocusBox: {
-    position: 'absolute',
-    height: 64,
-    width: 64,
-    borderRadius: 12,
-    borderWidth: 2,
-    borderColor: 'white',
-    opacity: 0.4,
-  },
-  flipText: {
-    color: 'white',
-    fontSize: 15,
-  },
-  zoomText: {
-    position: 'absolute',
-    bottom: 70,
-    zIndex: 2,
-    left: 2,
-  },
-  picButton: {
-    backgroundColor: 'darkseagreen',
-  },
-  landmark: {
-    width: landmarkSize,
-    height: landmarkSize,
-    position: 'absolute',
-    backgroundColor: 'red',
-  },
-  text: {
+  capture: {
+    flex: 0,
+    backgroundColor: '#fff',
+    borderRadius: 5,
+    color: '#000',
     padding: 10,
-    borderWidth: 2,
-    borderRadius: 2,
-    position: 'absolute',
-    borderColor: '#F00',
-    justifyContent: 'center',
+    margin: 40,
   },
-  textBlock: {
-    color: '#F00',
-    position: 'absolute',
-    textAlign: 'center',
-    backgroundColor: 'transparent',
+  circle: {
+    width: (Dimensions.get('window').width - 100),
+    height: (Dimensions.get('window').height / 2),
+    borderRadius: (Dimensions.get('window').width - 100) / 2,
+    borderWidth: 1,
+    borderColor: 'black',
   },
 });
 
-
-            /*<TouchableOpacity
-              style={[styles.flipButton, { flex: 0.25, alignSelf: 'flex-end' }]}
-              onPress={this.toggleFocus.bind(this)}
-            >
-              <Text style={styles.flipText}> AF : {this.state.autoFocus} </Text>
-            </TouchableOpacity>*/
-
-            /*
-            import * as Progress from 'react-native-progress';
-                              <View>
-                                <Progress.Circle
-                                  style={{alignItems: 'center'}}
-                                  size={400}
-                                  thickness ={30}
-                                  color={'#431540'}
-                                  indeterminate={true}
-                                  direction="counter-clockwise"
-                                />
-                              </View>
-            */
-
-            /*
-            <Text style={styles.capture} onPress={this.takePicture.bind(this)}> CAPTURE </Text>
-            */
+export default CameraScreen;
